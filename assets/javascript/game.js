68bc8f59ba0dead4f41bed9755b921ced7850303
Maeli Hector <maelihector@gmail.com>
2018-03-26 14:31:16 -0500

    Add words to play with matching images from nasa.gov.

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+// Hangman object to hold all logic and variables
+var cosmosHangman = {
+
+    //Object of all words used
+    wordsUsed: {
+        carlSagan: {
+            word: "Carl Sagan",
+            picture: "../images/CarlSagan.tiff",
+            hint: "He was the orginal host of the Cosmos documentary."
+        },
+        neilTyson: {
+            word: "Neil deGrasse Tyson",
+            picture: "../images/neil.jpg",
+            hint: "He is the current host of the Cosmos documentary."
+        },
+        jupiter: {
+            word: "Jupiter",
+            picture: "../images/jupiter.jpg",
+            hint: "This planet has the most mass of all the planets in our Solar System."
+        },
+        milkyWay: {
+            word: "Milky Way Galaxy",
+            picture: "../images/milkyway.jpg",
+            hint: "The name of the galaxy we live in."
+        },
+        supernova: {
+            word: "Supernova",
+            picture: "../images/supernova.jpg",
+            hint: "The violent death of a giant star"
+        },
+        titan: {
+            word: "Titan",
+            picture: "../images/titan.jpg",
+            hint: "Saturn's largest moon, which is said to have the most chance of containing life."
+        },
+        asteroid: {
+            word: "Asteroid",
+            picture: "../images/asteroid.jpg",
+            hint: "What is left of comets after their ice is evaporated by the sun."
+        },
+        halley: {
+            word: "Halley's Comet",
+            picture: "../images/halley.jpg",
+            hint: "The comet that rotates around the sun every 76 years."
+        },
+        proximaCentauri: {
+            word: "Proxima Centauri",
+            picture: "../images/proximaCentauri.jpg",
+            hint: "The name of the nearest star to our sun"
+        },
+        eventHorizon: {
+            word: "Event Horizon",
+            picture: "../images/eventHorizon.jpg",
+            hint: "The boundary that separates a black hole from the rest of the universe."
+        },
+    }
+};
