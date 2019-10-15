be7a47cd649710ee01e5045e8f593205de0b1a61
mhector <maelihector@gmail.com>
2019-10-15 09:20:27 -0500

    Add image alts. Edit some comments. Remove pseudocode.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,67 @@
 // cosmosGuessTheWordGame object to hold all logic and variables
 var cosmosGuessTheWordGame = {
 
-  //Object of all words, plus their picture and hint that will be used in the game
+  // Object of all words
   words: {
     "carl sagan": {
       picture: "CarlSagan.jpg",
-      hint: "He was the host of the orginal Cosmos documentary."
+      hint: "He was the host of the orginal Cosmos documentary.",
+      imageAlt: "A male astronomer in mid speech with images of planets in the background."
     },
     "neil degrasse tyson": {
       picture: "neil.jpg",
-      hint: "He is the current host of the Cosmos documentary."
+      hint: "He is the current host of the Cosmos documentary.",
+      imageAlt: "A male astronomer in black suit, inside a building with a James Webb Space Telescope poster hanging in the background."
     },
     "jupiter": {
       picture: "jupiter.jpg",
-      hint: "This planet has the most mass of all the planets in our Solar System."
+      hint: "This planet has the most mass of all the planets in our Solar System.",
+      imageAlt: "A planet with blue lights on its north pole, and a large red storm near the center."
     },
     "milky way galaxy": {
       picture: "milkyway.jpg",
-      hint: "The name of the galaxy we live in."
+      hint: "The name of the galaxy we live in.",
+      imageAlt: "A bright swirling galaxy."
     },
     "supernova": {
       picture: "supernova.jpg",
-      hint: "The violent death of a giant star"
+      hint: "The violent death of a giant star",
+      imageAlt: "A large, bright and colorful celestial explosion."
     },
     "titan": {
       picture: "titan.jpg",
-      hint: "Saturn's largest moon."
+      hint: "Saturn's largest moon.",
+      imageAlt: "A blue and light-green hued moon."
     },
     "asteroid": {
       picture: "asteroid.jpg",
-      hint: "What is left of comets after their ice is evaporated by the sun."
+      hint: "What is left of comets after their ice is evaporated by the sun.",
+      imageAlt: "A massive gray rock-like object with craters, flying through space."
     },
     "halleys comet": {
       picture: "halley.jpg",
-      hint: "The comet that rotates around the sun every 76 years."
+      hint: "The comet that rotates around the sun every 76 years.",
+      imageAlt: "A bright comet against a starry background."
     },
     "proxima centauri": {
       picture: "proximaCentauri.jpg",
-      hint: "The name of the star nearest to our sun"
+      hint: "The name of the star nearest to our sun",
+      imageAlt: "A large bright star among many small dimmed stars."
     },
     "event horizon": {
       picture: "eventHorizon.jpg",
-      hint: "The boundary that separates a black hole from the rest of the universe."
+      hint: "The boundary that separates a black hole from the rest of the universe.",
+      imageAlt: "A celestial body with two bright, intense lights shining from opposite sides."
     },
     "tardigrade": {
       picture: "tardigrade.jpg",
-      hint: "A species on earth that has lived over 500 million years."
+      hint: "A species on earth that has lived over 500 million years.",
+      imageAlt: "A microscopic view of a gray mite-like species."
     },
     "tiktaalik": {
       picture: "tiktaalik.jpg",
-      hint: "One of the first animals to venture onto land from the sea."
+      hint: "One of the first animals to venture onto land from the sea.",
+      imageAlt: "A drawn depiction of a long fish-like animal, half inside the water and half on land."
     }
   },
 
@@ -59,136 +71,136 @@ var cosmosGuessTheWordGame = {
   wins: 0,
   losses: 0,
   currentHint: null,
-  wordsAlreadyPlayed: [],
+  wordsPlayed: [],
 
-  // Method to play a new word.
+  hintDiv: document.querySelector("#hint"),
+
+  // Method to play a new word
   newWord: function () {
-    // Update player's current scores.
-    document.querySelector("#wins").innerHTML = this.wins;
-    document.querySelector("#totalWordsPlayed").innerHTML = this.wins + this.losses;
-    // Hide last hint if need be.
+    // Update score
+    this.updateDOM();
+    // Remove previous word's hint
     document.querySelector("#hint").innerHTML = "";
-    // Reset for a new word.
+    // Reset for a new word
     this.currentWord = null,
       this.wordItems = [],
       this.lettersGuessedArray = [],
       this.currentHint = null,
-
-      // Set value of 'gameWords' variable to 'words' in our 'cosmosGuessTheWordGame' object.
-      gameWords = Object.keys(this.words);
-    // Pick a random word to play and set the result as the value of 'this.currentWord'.
-    this.currentWord = gameWords[Math.floor(Math.random() * gameWords.length)];
-    // Check if word has already been played, of so pick a new word.
-    if (this.wordsAlreadyPlayed.indexOf(this.currentWord) > -1) this.newWord();
-    // else add the word to wordsAlreadyPlayed array.
-    else this.wordsAlreadyPlayed.push(this.currentWord);
-    // Slpit the string into an array and set the result as the value of 'this.wordItems'.
+      // Pick a random word to play
+      this.currentWord = Object.keys(this.words)[Math.floor(Math.random() * Object.keys(this.words).length)];
+    // Check if word has already been played in current 5-game set, if so pick a new word, else add the word to wordsPlayed array
+    if (this.wordsPlayed.indexOf(this.currentWord) > -1) this.newWord();
+    else this.wordsPlayed.push(this.currentWord);
+    // Split the word string into an array
     this.wordItems = this.currentWord.split("");
-    // Give the global var the current hint.
+    // Update word hint
     this.currentHint = this.words[this.currentWord].hint;
-    // Update the image of the word.
-    document.querySelector("#image").innerHTML = "<img class='hint-image' src='./assets/images/" + this.words[this.currentWord].picture + "' alt='image of hidden word' />"
-
+    // Update word image
+    document.querySelector("#image").innerHTML = "<img class='hint-image' src='./assets/images/" + this.words[this.currentWord].picture + "' alt='" + this.words[this.currentWord].imageAlt + "' />"
+    // Call method to build word blanks
     this.buildWordBlanks();
   },
 
-  avoidRepeatWords: function (word) {
 
-    if (this.wordsAlreadyPlayed.indexOf(word) > -1) return;
+  avoidRepeatWords: function (word) {
+    if (this.wordsPlayed.indexOf(word) > -1) return;
   },
 
-  // Method to build the current word at play on the DOM. 
+  // Method to build the current word at play
   buildWordBlanks: function () {
-    // Start with an empty string, since outcome will be a string (of letters, whitespaces, and underscores).
+    // Start with an empty string, since outcome will be a string (of letters, whitespaces, and underscores)
     var wordBlanks = "";
-    // Loop through 'this.wordItems' array.
+    // Loop through word letters
     for (var i = 0; i < this.wordItems.length; i++) {
-      // If there is a whitespace, make sure to add a space.
+      // Replace whitespaces with a space
       if (this.wordItems[i] === " ") {
         wordBlanks += "  ";
-        // Check 'this.lettersGuessedArray' against 'this.wordItems' for matches to fill correctly guessed letters.
+        // Leave letters as is if they are in `lettersGuessedArray`
       } else if (this.lettersGuessedArray.indexOf(this.wordItems[i]) !== -1) {
         wordBlanks += this.wordItems[i];
       } else {
-        // Put underscores for every item left of 'this.wordItems'.
+        // Replace unguessed letters with underscores
         wordBlanks += "&nbsp;_&nbsp;";
       }
     }
-    // Access the DOM to 'draw' our wordBlanks.
+    // Dump the newly builded word to DOM
     document.querySelector("#currentWord").innerHTML = wordBlanks;
-    // Call 'checkTotalCorrectGuesses' method
+
     this.checkTotalCorrectGuesses();
   },
 
-  // Method that checks if player has guessed the entire word.
-  // Pass the method two arguments, one being the letters of the word in play, the other being the player's guesses.
+  // Method that checks if player has guessed the entire word
   checkTotalCorrectGuesses: function (uniqueLettersOfTheWord, totalGuessedLetters) {
-    // First filter/remove whitespace from the word in play
+    // Remove whitespace from the word in play
     lettersOfTheWord = this.wordItems.filter(function (str) {
       return /\S/.test(str);
     });
-    // Then filter repeating letters of the word and place new array in var 'uniqueletterOfTheWord'.
+    // Remove repeating letters of the word
     var uniqueLettersOfTheWord = lettersOfTheWord.filter(function (item, index) {
       return lettersOfTheWord.indexOf(item) >= index;
     });
-    // Grab the value of global var 'this.lettersGuessedArray'.  
+    // Grab letters already guessed
     totalGuessedLetters = this.lettersGuessedArray;
 
-    // Start with an empty array for 'correctGuesses'
+    // Start with an empty array for correct letter guesses
     var correctGuesses = [];
 
-    // Compare the 'totalGuessedLetters' and  'uniqueletterOfTheWord' arrays
+    // Compare guessed letters and letters of the word
     totalGuessedLetters.forEach((guessedLetter) => uniqueLettersOfTheWord.forEach((uniqueLetter) => {
-      // If a guessed letter is equal to a unique letter in the word AND if it's not in there already (to prevent ducplicates if a word has more than one of the same letter),
+      // If a guessed letter is equal to a letter in the word AND has not been compared already (prevents ducplicates for words using the same letter more than once), push to correctGuesses array
       if (guessedLetter === uniqueLetter && correctGuesses.indexOf(guessedLetter) === -1) {
-        // push to correctGuesses array
         correctGuesses.push(guessedLetter)
         return correctGuesses;
       }
     }));
 
-    // Update the DOM with  wrong guesses amount.
-    wrongGuesses = this.lettersGuessedArray.length - correctGuesses.length;
+    // Update the DOM with  wrong guesses amount
+    var wrongGuesses = this.lettersGuessedArray.length - correctGuesses.length;
     document.querySelector("#numberOfIncorrectGuesses").innerHTML = wrongGuesses;
 
     // Call diff() function to get incorrect letters guessed.
     var incorrectGuesses = this.diff(this.lettersGuessedArray, correctGuesses);
+
     // Update the DOM to show the player right and wrong guessed letters.
     document.querySelector("#wrongLettersGuessed").innerHTML = incorrectGuesses;
     document.querySelector("#rightLettersGuessed").innerHTML = correctGuesses;
 
-    // Check if player has reached their limit to wrong guesses.
+    // If player has reached their limit of wrong guesses
     if (wrongGuesses === 10) {
-      // increase losses by +1
+      // Increase losses by 1
       this.losses++;
-      // reset wrongGuesses to 0
+      this.updateDOM();
+      // Reset wrongGuesses to 0
       wrongGuesses = 0;
       this.checkGameOver();
-      // Or if player has completed the word.
+      // Else if player has completed the word.
     } else if (correctGuesses.length === uniqueLettersOfTheWord.length) {
-      // increase wins by +1
+      // Increase wins by 1
       this.wins++;
-      // reset wrongGuesses to 0
+      this.updateDOM();
+      // Reset wrongGuesses to 0
       wrongGuesses = 0;
       this.checkGameOver();
     }
   },
 
   // Method that returns incorrectly guessed letters by comparing correct guesses array and total guesses array
-  diff: function(totalGuessesArray, correctGuessesArray) {
-    // filter total guesses array and return only those that do not appear in correct guesses array.
-    return totalGuessesArray.filter(function(i) {return correctGuessesArray.indexOf(i) < 0;});
+  diff: function (totalGuessesArray, correctGuessesArray) {
+    // Filter total guesses array and return only those that do not appear in correct guesses array
+    return totalGuessesArray.filter(function (i) {
+      return correctGuessesArray.indexOf(i) < 0;
+    });
   },
 
-  // Method to check if player has played 5 games and whether they got a perfect score or not.
+  // Method to check if player has played 5 games and whether they got a perfect score or not
   checkGameOver: function () {
-    totalGames = this.wins + this.losses;
+    var totalGames = this.wins + this.losses;
 
-    // If player has played 5 games total, reset wins and losses to 0;
+    // If player has played 5 games total, reset wins and losses to 0
     if (this.wins === 5) {
       this.wins = 0;
       this.losses = 0;
-      // Congratulate if player won 5 of 5.
+      // Congratulate if player won 5 of 5
       setTimeout(function () {
         $('#perfectScoreModal').modal('show');
       }, 1000);
@@ -196,7 +208,7 @@ var cosmosGuessTheWordGame = {
     } else if (totalGames === 5) {
       this.wins = 0;
       this.losses = 0;
-      // Tell player they did not get 5 of 5.
+      // Tell player they did not get 5 of 5
       setTimeout(function () {
         $('#fiveWordsPlayedModal').modal('show');
       }, 1000);
@@ -208,27 +220,25 @@ var cosmosGuessTheWordGame = {
 
   // This method makes sure that player's only play letter keys
   acceptOnlyAlphabetkeys: function (key) {
-    alphabetOnly = key.match(/[a-z]/);
-    // If player presses something other than an alphabet key,
+    var alphabetOnly = key.match(/[a-z]/);
+    // If player presses something other than an alphabet key, tell player to press only letters
     if (alphabetOnly === null || alphabetOnly.input.length > 1) {
-      //  tell player to press a only letters via modal.
       $('#lettersOnlyModal').modal('show');
     } else {
-      // If a letter is pressed, pass the guessed letter into the checkForDuplicateLetters method.
+      // Else pass the guessed letter into the `checkForDuplicateLetters` method
       this.checkForDuplicateLetters(alphabetOnly.input);
     }
   },
 
-  // This method checks if the played letter is not already in the lettersGuessedArray to keep the player from losing a guess.
+  // Method that checks if the played letter is not already in `lettersGuessedArray` to keep the player from losing a guess
   checkForDuplicateLetters: function (letter) {
-    // If letter is not (-1) in our lettersGuessedArray,
+    // If letter is not in `lettersGuessedArray`, push the letter to `lettersGuessedArray` 
     if (this.lettersGuessedArray.indexOf(letter) === -1) {
-      // push the letter to lettersGuessedArray 
       this.lettersGuessedArray.push(letter);
-      // call 'buildWordBlanks' metthod.
+      // Build word
       this.buildWordBlanks();
     } else {
-      // Tell player that letter has already been played.
+      // Else tell player that the letter has already been played
       $('#duplicate-letter').text(letter);
       $('#noDuplicatesModal').modal('show');
     }
@@ -236,99 +246,29 @@ var cosmosGuessTheWordGame = {
 
   // Method that displays text hint when player click on the 'hint button'
   getHint: function () {
-    // Access the target aread of the DOM
-    var hintDiv = document.querySelector("#hint");
-    // Display text in target area.
-    hintDiv.innerHTML = this.currentHint;
+    this.hintDiv.innerHTML = this.currentHint;
   },
 
+  updateDOM: function () {
+    // Update player's current scores
+    document.querySelector("#wins").innerHTML = this.wins;
+    document.querySelector("#totalWordsPlayed").innerHTML = this.wins + this.losses;
+  }
+
 }
 
-// Start game at page load.
+// Start game at page load
 cosmosGuessTheWordGame.newWord();
 
-
-// Capture the players 'onkeyup' letter key
+// Capture the players `onkeyup` letter key
 document.onkeyup = function (event) {
-  keyPressed = event.key;
-  // Pass keyPressed value to our acceptOnlyAlphabetkeys method.
+  var keyPressed = event.key;
+  // Check if key pressed is alpha
   cosmosGuessTheWordGame.acceptOnlyAlphabetkeys(keyPressed);
-}
+};
 
 // Listen for hint button clicks
 var btn = document.getElementById("getHint");
 btn.onclick = function (event) {
   cosmosGuessTheWordGame.getHint(event);
 };
-
-/* 
-
-Hangman Pseudocode 
-
-The player clicks on letters of their keyboard they think will complete the word in play.
-
-What the page will show the player:
-1. '0 0f 0' 'wins' of 'total words played'. Tell player they will only play 5 games (5 of 5 is perfect score).
-2. 10 as limit to 'number of incorrect guesses'. Will decrease -1 as player guesses a wrong letter.
-3. List of ALL 'Letters Guessed'. *Will not allow the player to guess the same letter twice to as not lose a guess on duplicates.
-4. Show the number of blanks as underscores( _ ) for every letter of the current word in play, 
-   and a single white space for spaces in between words..
-5.Show a hint button, and have it blink lightly when the player has only 2 guesses left.
-
-For every letter clicked/guessed while word is in play:
-    1. Check 'lettersGuessed' array for duplicates: 
-        A. If negative match to 'lettersGuessed' array:
-            1. Push letter to 'lettersGuessed' array.
-            2. Check 'wordInPlay' array to see if there is a letter/item match.
-                a. If positive match of a 'wordInPlay' array item:
-                    1. Replace the appropriate _ with said letter.
-                    2. Check 'wordInPlay' to see if word is complete:
-                        I. If word is complete:
-                            1. Have modal show player won that game by completing the word.
-                            2. Show photo of corresponding word.
-                            3. +1 on 'wins'.
-                            4. +1 on 'totalWordsPlayed'.
-                                a. If 'totalWordsPlayed' = 10:
-                                    1. Show modal to tell player that all words have been played, show final score.
-                                    2. Reset whole Game.
-                                b. If 'totalWordsPlayed' < 10:
-                                    1. Play next word.
-                        II. If word not complete:
-                            1. Wait for next guess.
-                b. If negative match to 'wordInPlay' array item:
-                    1. Decrease by -1 on 'incorrectGuesses'.
-                        I. If 'incorrectGuesses' is < 0:
-                            1. Wait for next guess.
-                        II. If 'incorrectGuesses' < 3 && > 0:
-                            1. Blink the Hint Button *if hint is not already displayed.
-                            2. Wait for next guess.
-                        III. If 'incorrectGuesses' = 0:
-                            1. Have modal show player that they have 0 guesses left so they lost that game.
-                            2. +1 on 'totalWordsPlayed'.
-                                a. If 'totalWordsPlayed' = 10:
-                                    1. Show modal to tell player that all words have been played, show final score.
-                                    2. Reset whole Game.
-                                b. If 'totalWordsPlayed' < 10:
-                                    1. Play next word.
-        B. If positive match to 'lettersGuessed' array:
-            1. Have modal tell player 'You have already guessed _ , guess a new letter.'
-            2. Wait for next guess.
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
